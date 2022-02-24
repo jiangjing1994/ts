a1c7b5170e1e3bd61bb777e8319f3b1f6466fefc
JiangJing <jiangjing1@yusys.com.cn>
2022-02-24 11:05:07 +0800

    feat: test

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -32,24 +32,9 @@
         <div class="flex w-full h-full py-5 xl:h-auto xl:py-0 xl:my-0 xl:w-6/12">
           <div
             :class="`${prefixCls}-form`"
-            class="
-              relative
-              w-full
-              px-5
-              py-8
-              mx-auto
-              my-auto
-              rounded-md
-              shadow-md
-              xl:ml-16 xl:bg-transparent
-              sm:px-8
-              xl:p-4 xl:shadow-none
-              sm:w-3/4
-              lg:w-2/4
-              xl:w-auto
-              enter-x
-            "
+            class="relative w-full px-5 py-8 mx-auto my-auto rounded-md shadow-md xl:ml-16 xl:bg-transparent sm:px-8 xl:p-4 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto enter-x"
           >
+            <Test :loading="false" />
             <LoginForm />
             <ForgetPasswordForm />
             <RegisterForm />
@@ -70,6 +55,7 @@
   import RegisterForm from './RegisterForm.vue';
   import MobileForm from './MobileForm.vue';
   import QrCodeForm from './QrCodeForm.vue';
+  import Test from './Test.vue';
   import { useGlobSetting } from '/@/hooks/setting';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { useDesign } from '/@/hooks/web/useDesign';
---
+++ src/views/sys/login/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>12334455 {{ loading }}</div>
+  <div>12334455 {{ titile }}</div>
+</template>
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  const props = defineProps({
+    loading: {
+      type: Boolean,
+    },
+  });
+  const titile = computed(() => {
+    return props.loading;
+  });
+</script>
